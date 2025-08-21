26bb1fa1583a26a7b853f2091d25e754a5049a9d
FacundoZucchi <facundo.zucchi@davinci.edu.ar>
2025-08-21 16:18:37 -0300

    Menu Principal
    - Animaciones

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,6 +9,7 @@ public class MainMenuManager : MonoBehaviour
 {
     private GameObject _mainMenu;
     private GameObject _optionMenu;
+    private GameObject _creditsMenu;
     private Image _fadePanel;
     public bool _fadeOnOff;
     [SerializeField] private float _fadeSpeed;
@@ -24,12 +26,15 @@ public class MainMenuManager : MonoBehaviour
     {
         _mainMenu = GameObject.FindGameObjectWithTag("MainMenu");
         _optionMenu = GameObject.FindGameObjectWithTag("OptionsMenu");
+        _creditsMenu = GameObject.FindGameObjectWithTag("CreditsMenu");
         _fadePanel = GameObject.FindGameObjectWithTag("fadePanel").GetComponent<Image>();
     }
+
     private void Start()
     {
         _mainMenu.SetActive(true);
         _optionMenu.SetActive(false);
+        _creditsMenu.SetActive(false);
         _fadeOnOff = true;
     }
 
@@ -65,19 +70,48 @@ public class MainMenuManager : MonoBehaviour
         _fadeOnOff = true;
     }
 
-    public void BackBTN()
+    public void CreditsBTN()
     {
-        StartCoroutine(FadeBack());
+        StartCoroutine(FadeCredits());
     }
 
-    private IEnumerator FadeBack()
+    private IEnumerator FadeCredits()
+    {
+        _fadeOnOff = false;
+        yield return new WaitForSeconds(_optionsWaitTime);
+        _mainMenu.SetActive(false);
+        _creditsMenu.SetActive(true);
+        _fadeOnOff = true;
+    }
+
+    public void BackBTNOptions()
+    {
+        StartCoroutine(FadeBackOptions());
+    }
+
+    private IEnumerator FadeBackOptions()
     {
         _fadeOnOff = false;
         yield return new WaitForSeconds(_backWaitTime);
         _mainMenu.SetActive(true);
         _optionMenu.SetActive(false);
-        _fadeOnOff=true;
+        _fadeOnOff = true;
     }
+
+    public void BackBTNCredits()
+    {
+        StartCoroutine(FadeBackCredits());
+    }
+
+    private IEnumerator FadeBackCredits()
+    {
+        _fadeOnOff = false;
+        yield return new WaitForSeconds(_backWaitTime);
+        _mainMenu.SetActive(true);
+        _creditsMenu.SetActive(false);
+        _fadeOnOff = true;
+    }
+    
 
     public void ExitBTN()
     {
